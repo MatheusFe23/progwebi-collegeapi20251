a4296a9119337fedb78322314fe38021f2fd2056
MatheusFe23 <155886669+MatheusFe23@users.noreply.github.com>
2025-03-14 22:16:05 -0300

    Add files via upload

---
+++ src/main/java/br/ueg/progwebi/collegeapi/model/Student.java
@@ -0,0 +1,21 @@
+package br.ueg.progwebi.collegeapi.model;
+
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Student {
+    private Long id;
+    private String name;
+    private String registerNumber;
+    private String course;
+    private LocalDate registerDate;
+}
